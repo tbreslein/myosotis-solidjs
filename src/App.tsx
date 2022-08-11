432007321ec09cd827f6c1916069fd190b58725d
tbreslein <tommy.breslein@protonmail.com>
2022-08-11 22:27:57 +0200

    ENH|started playing around with SUID cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import Card from "@suid/material/Card";
+import CardContent from "@suid/material/CardContent";
+import Typography from "@suid/material/Typography";
 import type { Component } from "solid-js";
 
 const App: Component = () => {
-  return <h1>hello solidjs!!!</h1>;
+  return (
+    <>
+      <h1>hello solidjs!!!</h1>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Do the dishes
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            I should really do the dishes.
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default App;
