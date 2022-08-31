3f84966c855b2d0738dbe14f4095a8bfd17c652c
tbreslein <tommy.breslein@protonmail.com>
2022-08-31 22:10:45 +0200

    style(Menubar): extracted signals and callbacks

--- src/components/Menubar/index.tsx
+++ src/components/Menubar/index.tsx
@@ -8,38 +8,36 @@ import { createSignal, Show } from "solid-js";
 import Drawer from "@suid/material/Drawer";
 import NavList from "./NavList";
 
-const Menubar = () => {
-  const [drawerVisible, setDrawerVisible] = createSignal(false);
-  const toggleDrawer = () => {
-    setDrawerVisible(!drawerVisible());
-  };
-
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Show when={drawerVisible()}>
-        <Drawer onClose={toggleDrawer} open={drawerVisible()}>
-          <NavList />
-        </Drawer>
-      </Show>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={toggleDrawer}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Myosotis
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+const [drawerVisible, setDrawerVisible] = createSignal(false);
+const toggleDrawer = () => {
+  setDrawerVisible(!drawerVisible());
 };
+
+const Menubar = () => (
+  <Box sx={{ flexGrow: 1 }}>
+    <Show when={drawerVisible()}>
+      <Drawer onClose={toggleDrawer} open={drawerVisible()}>
+        <NavList />
+      </Drawer>
+    </Show>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={toggleDrawer}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Myosotis
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
 
 export default Menubar;
