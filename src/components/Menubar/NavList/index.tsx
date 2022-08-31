6685a51a817c103897e423bd89c741fd265901ca
tbreslein <tommy.breslein@protonmail.com>
2022-08-31 14:27:42 +0200

    refactor(NavList): abstracted nested NavLists

--- src/components/Menubar/NavList/index.tsx
+++ src/components/Menubar/NavList/index.tsx
@@ -1,64 +1,75 @@
 import Box from "@suid/material/Box";
+import AssignmentIcon from "@suid/icons-material/Assignment";
+import HourglassButtomIcon from "@suid/icons-material/HourglassBottom";
 import HouseIcon from "@suid/icons-material/House";
 import SettingsIcon from "@suid/icons-material/Settings";
+import SwapVertIcon from "@suid/icons-material/SwapVert";
 import List from "@suid/material/List";
 import ListItem from "@suid/material/ListItem";
 import ListItemButton from "@suid/material/ListItemButton";
 import ListItemIcon from "@suid/material/ListItemIcon";
 import ListItemText from "@suid/material/ListItemText";
 import Divider from "@suid/material/Divider";
-import { mapArray } from "solid-js";
+import { Component, For, JSXElement, Show } from "solid-js";
 
-const primaryNavItems = [
-  {
-    icon: <HouseIcon />,
-    text: "Home",
-  },
-  {
-    icon: <SettingsIcon />,
-    text: "Settings",
-  },
+type NavItem = {
+  icon: JSXElement;
+  text: string;
+};
+
+const navItems: NavItem[][] = [
+  [
+    {
+      icon: <AssignmentIcon />,
+      text: "My ToDos",
+    },
+    {
+      icon: <HourglassButtomIcon />,
+      text: "Deadlines",
+    },
+  ],
+  [
+    {
+      icon: <HouseIcon />,
+      text: "Manage Household",
+    },
+    {
+      icon: <SwapVertIcon />,
+      text: "Switch Household",
+    },
+  ],
+  [
+    {
+      icon: <SettingsIcon />,
+      text: "Settings",
+    },
+  ],
 ];
 
-const secondaryNavItems = [
-  {
-    icon: <HouseIcon />,
-    text: "Home",
-  },
-  {
-    icon: <SettingsIcon />,
-    text: "Settings",
-  },
-];
+const NavListItem: Component<NavItem> = props => (
+  <ListItem disablePadding>
+    <ListItemButton>
+      <ListItemIcon>{props.icon}</ListItemIcon>
+      <ListItemText primary={props.text} />
+    </ListItemButton>
+  </ListItem>
+);
 
 const NavList = () => (
   <Box sx={{ width: "auto" }}>
     <List>
-      {mapArray(
-        () => primaryNavItems,
-        navItem => (
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>{navItem.icon}</ListItemIcon>
-              <ListItemText primary={navItem.text} />
-            </ListItemButton>
-          </ListItem>
-        )
-      )}
-    </List>
-    <Divider />
-    <List>
-      {mapArray(
-        () => secondaryNavItems,
-        navItem => (
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>{navItem.icon}</ListItemIcon>
-              <ListItemText primary={navItem.text} />
-            </ListItemButton>
-          </ListItem>
-        )
-      )}
+      <For each={navItems}>
+        {(subList, index) => (
+          <>
+            <For each={subList}>
+              {navItem => <NavListItem icon={navItem.icon} text={navItem.text} />}
+            </For>
+            <Show when={index() < navItems.length - 1}>
+              <Divider />
+            </Show>
+          </>
+        )}
+      </For>
     </List>
   </Box>
 );
