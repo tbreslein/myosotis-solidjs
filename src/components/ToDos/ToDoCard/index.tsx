fe1c1de250120466caa5b8304fbc169a0e6798c5
tbreslein <tommy.breslein@protonmail.com>
2022-09-01 10:05:14 +0200

    feat(ToDoCard): add checkbox

--- src/components/ToDos/ToDoCard/index.tsx
+++ src/components/ToDos/ToDoCard/index.tsx
@@ -1,19 +1,24 @@
+import Box from "@suid/material/Box";
 import Card from "@suid/material/Card";
 import CardContent from "@suid/material/CardContent";
+import Checkbox from "@suid/material/Checkbox";
 import Typography from "@suid/material/Typography";
 import { Component } from "solid-js";
 import { ToDo } from "../../../types/ToDo";
 
 const ToDoCard: Component<ToDo> = props => (
-  <Card sx={{ maxWidth: 345 }}>
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        {props.title}
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        {props.description}
-      </Typography>
-    </CardContent>
+  <Card sx={{ maxWidth: 345, display: "flex" }}>
+    <Box sx={{ display: "flex", flexDirection: "row" }}>
+      <CardContent sx={{ flex: "1 0 auto" }}>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.description}
+        </Typography>
+      </CardContent>
+      <Checkbox checked={props.done} inputProps={{ "aria-label": "todo is done" }} />
+    </Box>
   </Card>
 );
 
